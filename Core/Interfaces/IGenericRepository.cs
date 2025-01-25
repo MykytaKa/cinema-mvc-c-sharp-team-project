9ce9e492d701d8780062a2200c1970f08d1ad8ce
konoplytska <146989310+konoplytska@users.noreply.github.com>
2025-01-25 17:01:01 +0200

    Update IGenericRepository.cs

--- Core/Interfaces/IGenericRepository.cs
+++ Core/Interfaces/IGenericRepository.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Linq.Expressions;
@@ -15,14 +15,6 @@ namespace Core.Interfaces
             Func<IQueryable<TEntity>, IOrderedQueryable<TEntity>> orderBy = null,
             string includeProperties = "");
 
-       TEntity GetByID(object id);
-       IEnumerable<TEntity> GetAll();
-       void Insert(TEntity entity);
-       void Update(TEntity entity);
-       void Delete(object id);
-       void Delete(TEntity entity);
-       Task<IEnumerable<TEntity>> GetAsync(Expression<Func<TEntity, bool>> filter = null);
-       Task AddAsync(TEntity entity);
         Task<TEntity> GetByIDAsync(object id);
 
         Task<IEnumerable<TEntity>> GetAllAsync();
@@ -34,7 +26,5 @@ namespace Core.Interfaces
         Task DeleteAsync(object id);
 
         Task DeleteAsync(TEntity entity);
-
     }
-
 }

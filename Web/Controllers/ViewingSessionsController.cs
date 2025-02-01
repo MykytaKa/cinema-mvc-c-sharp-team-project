52dae4543c055a78fe0b78f5488240a2f5f0f4f0
shyga <shygaeg@gmail.com>
2025-02-01 23:38:34 +0200

    fix filtration

--- Web/Controllers/ViewingSessionsController.cs
+++ Web/Controllers/ViewingSessionsController.cs
@@ -15,28 +15,10 @@ public class ViewingSessionsController : Controller
 
     public async Task<IActionResult> ViewingSessions(SessionFilterModel filter)
     {
-        if (filter.SessionDate == null)
-        {
-            filter.SessionDate = DateTime.Today;
-        }
-        // Отримання відфільтрованих сеансів через сервісний шар
-        var sessions = await _sessionService.GetFilteredSessionsAsync(filter);
-
-        // Вибираємо унікальні фільми зі списку сеансів
-        var uniqueFilms = sessions
-            .GroupBy(s => s.Film.Id)
-            .Select(g =>
-            {
-                var film = g.First().Film;
-                film.Sessions = g.ToList(); // Додаємо тільки відфільтровані сеанси
-                return film;
-            })
-            .ToList();
-        
-        // Виклик сервісів для отримання жанрів та вікових рейтингів
+        // Виклик сервісів
         filter.AvailableGenres = (await _sessionService.GetAllGenresAsync()).ToList();
         filter.AvailableAgeRatings = (await _sessionService.GetAllAgeRatingsAsync()).ToList();
-        filter.Films = uniqueFilms;
+        filter = await _sessionService.GetFilteredFilmsAsync(filter);
         return View(filter);
     }
 }

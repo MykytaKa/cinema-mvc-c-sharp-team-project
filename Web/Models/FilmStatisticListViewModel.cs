c2d7f51472093b4dd66ad6283a0219963f7bc331
Dima Safonchyk <safonchyk@ukr.net>
2025-02-08 17:06:08 +0200

    Create 2 statisic window

---
+++ Web/Models/FilmStatisticListViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+
+namespace Web.Models
+{
+    public class FilmStatisticListViewModel
+    {
+        public List<FilmStatisticViewModel> Statistics { get; set; }
+        public int CurrentPage { get; set; }
+        public int TotalPages { get; set; }
+        public string SortBy { get; set; }
+    
+    }
+}
